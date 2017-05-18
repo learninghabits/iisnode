b1942943cdf50a2487281221b663c0ca38788959
tshepontlhokoa <tshepo.ntlhokoa@gmail.com>
2017-05-18 16:13:13 +0200

    first commit

---
+++ code/after/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname));
+app.get('/', function (request, response) {
+	response.sendFile(__dirname + '/index.html');
+});
+var port = process.env.PORT || 8181;
+http.listen(port, function () {
+    console.log('LISTENING ON PORT: ' + port);    
+});
